77d31ea42e5b0392ae6f41b1163b7c07bbe1c389
Patrick Mick <patrick.mick@discordapp.com>
2023-11-28 11:36:30 -0800

    Fixup

--- android/src/newarch/java/com/reactnativecommunity/slider/ReactSliderManager.java
+++ android/src/newarch/java/com/reactnativecommunity/slider/ReactSliderManager.java
@@ -93,7 +93,7 @@ public class ReactSliderManager extends SimpleViewManager<ReactSlider> implement
               eventDispatcher.dispatchEvent(
                       new ReactSliderEvent(
                               reactTag,
-                              ((ReactSlider)seekbar).toRealProgress(seekbar.getProgress()));
+                              ((ReactSlider)seekbar).toRealProgress(seekbar.getProgress())));
             }
           };
 
